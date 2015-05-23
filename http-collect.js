f9d8e5ddee8fdb22e1f344f7e2e45d94b1b45467
Richard Hanna <richard.hanna@elao.com>
2015-05-23 14:48:27 +0200

    Add step 8: Http collect

---
+++ http-collect.js
@@ -0,0 +1,17 @@
+var http = require('http'),
+    bl   = require('bl'),
+    url  = process.argv[2];
+
+http.get(url, function (response) {
+    response.setEncoding('utf8');
+
+    response.pipe(bl(function (err, data) {
+        if (err) {
+            return console.error(err)
+        }
+
+        data = data.toString();
+        console.log(data.length);
+        console.log(data);
+    }));
+});
